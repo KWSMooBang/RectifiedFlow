d3ded701db1cf754f46f096a98b901a99b535d24
KWSMooBang <40kimwoosung@gmail.com>
2024-12-14 20:54:55 +0900

    update RectifiedFlow class

--- models/rectified_flow.py
+++ models/rectified_flow.py
@@ -6,6 +6,7 @@ import utils as mutils
 class RectifiedFlow():
     def __init__(
         self,
+        model,
         init_type='gaussian',
         noise_scale=1.0,
         reflow_flag=False,
@@ -15,17 +16,18 @@ class RectifiedFlow():
         sigma_var=0.0,
         ode_tol=1e-5,
         sample_N=None,
-    ):
-        if sample_N is not None:
-            self.sample_N = sample_N
-            print(f"Number of sampling steps: {self.sample_N}")
-        
+    ):        
+        self.model = model
         self.init_type = init_type
         self.noise_scale = noise_scale
         self.use_ode_sampler = use_ode_sampler
-        self.ode_tol = ode_tol
         self.sigma_t = lambda t: (1. - t) * sigma_var
+        self.ode_tol = ode_tol
         
+        if sample_N is not None:
+            self.sample_N = sample_N
+            print(f"Number of sampling steps: {self.sample_N}")
+                
         print(f"Initial distribution variance: {self.noise_scale}")
         print(f"SDE sampler variance: {sigma_var}")
         print(f"ODE tolerence: {self.ode_tol}")
@@ -46,7 +48,7 @@ class RectifiedFlow():
         return 1.
     
     @torch.no_grad()
-    def ode(self, init_input, model, reverse=False):
+    def ode(self, init_input, reverse=False):
         """
         run ODE solver for reflow.
         init_input can be pi_0 or pi_1
@@ -61,15 +63,15 @@ class RectifiedFlow():
         # initial sample
         x = init_input.detach().clone()
         
-        model_fn = mutils.get_model_fn(model, train=False)
+        self.model.eval()
         shape = init_input.shape
         device = init_input.device
         
         # dx/dt = v(x, t)
-        def ode_func(t, x):
+        def ode_func(x, t):
             x = mutils.from_flattened_numpy(x, shape).to(device).type(torch.float32)
-            vec_t = torch.ones(shape[0], device=x.device) * t
-            drift = model_fn(x, vec_t*999)
+            t = torch.ones(shape[0], device=x.device) * t
+            drift = self.model(x, t)
             return mutils.to_flattened_numpy(drift)
         
         # black-box ODE solver for the probability flow ODE
@@ -92,7 +94,7 @@ class RectifiedFlow():
         return x
     
     @torch.no_grad()
-    def euler_ode(self, init_input, model, reverse=False, N=100):
+    def euler_ode(self, init_input, reverse=False, N=100):
         """
         run euler ODE solver for reflow
         X_{t+1} = X_t + v(X_t, t) * 1 / N
@@ -104,22 +106,22 @@ class RectifiedFlow():
         # initial sample
         x = init_input.detach().clone()
         
-        model_fn = mutils.get_model_fn(model, train=False)
+        self.model.eval()
         shape = init_input.shape
         device = init_input.device
         
         for i in range(N):
-            num_t = i / N * (self.T - eps) + eps
-            t = torch.ones(shape[0], device=device) * num_t
-            pred = model_fn(x, t*999)
+            t = i / N * (self.T - eps) + eps
+            t = torch.ones(shape[0], device=device) * t
+            drift = self.model(x, t)
             
             # x_{t-1} = x_t + v(x, t) * dt
-            x = x.detach().clone() + pred * dt
+            x = x.detach().clone() + drift * dt
             
         return x
     
-    def get_z0(self, batch, train=True):
-        b, c, h, w = batch.shape
+    def get_z0(self, x):
+        b, c, h, w = x.shape
         
         if self.init_type == 'gaussian':
             current_shape = (b, c, h, w)
